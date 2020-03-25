db6513db5635e3871dabccae7219952e4f154082
Fabian Maeser <fabian.maeser@ergon.ch>
2020-03-25 14:31:03 +0100

    Arrow functions and implicit returns

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('Ahoy TypeScript!');
+const pizzas = [
+    {name: 'Pepperoni', toppings: ['pepperoni']}
+];
+
+const mappedPizzas = pizzas.map(
+    function(pizza) {
+        return pizza.name.toUpperCase();
+    }
+)
+
+console.log(pizzas);
+console.log(mappedPizzas);
