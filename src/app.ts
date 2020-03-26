792ea12424644da0ba2ff5e19636b4ef30cbd5b0
Fabian Maeser <fabian.maeser@ergon.ch>
2020-03-26 13:32:31 +0100

    Union and literal types

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
-// declare coupon of type string OR null
-// - this is required when setting (type checking) "strict": true
-// - actually legal is "... coupon: string | number", but does not make a lot of sense...
-let coupon: string | null = 'pizza25';
+let pizzaSize: string = 'medium';
 
-function removeCoupon(): void {
-    coupon = null;
+function selectSize(size: 'small' | 'medium' | 'large') {
+    pizzaSize = size;
 }
 
-console.log(coupon);
+selectSize('large');
 
-removeCoupon();
-console.log(coupon);
+console.log(`Pizza size = ${pizzaSize}`);
