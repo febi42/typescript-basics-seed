268543495e4a602006b5437649822299893f50f1
Fabian Maeser <fabian.maeser@ergon.ch>
2020-03-27 09:49:06 +0100

    Setters and Getters

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
+class Sizes {
+    constructor(public sizes: string[]) {}
+
+    get availableSizes() { return this.sizes; }
+
+    set availableSizes(sizes: string[]) { this.sizes = sizes; }
+}
+
+const sizes = new Sizes(['small', 'medium']);
+
+// we can now access 'availableSizes'
+// ... but also 'sizes' ?!
+// (cannot make 'sizes' private...)
+console.log(sizes.sizes);
+console.log(sizes.availableSizes);
+
+sizes.sizes = ['medium', 'large'];
+sizes.availableSizes = ['medium', 'large'];
+console.log(sizes.sizes);
+console.log(sizes.availableSizes);
+
 class Pizza {
     public toppings: string[] = [];
 
