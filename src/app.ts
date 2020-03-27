0c67d9aea9a6fcfb2075082825c874a7afa17d2b
Fabian Maeser <fabian.maeser@ergon.ch>
2020-03-27 14:01:09 +0100

    Protected Members and Inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-class Sizes {
-    constructor(public sizes: string[]) {}
+abstract class Sizes {
+    constructor(protected sizes: string[]) {}
 
     get availableSizes() { return this.sizes; }
 
@@ -11,13 +11,17 @@ class Pizza extends Sizes {
 
     constructor(
         readonly name: string,
-        public sizes: string[]
+        sizes: string[]
     ) {
         super(sizes);
     }
 
-    addTopping(topping: string) {
+    public addTopping(topping: string) {
         this.toppings.push(topping);
+    }
+
+    public updateSizes(sizes: string[]) {
+        this.sizes = sizes;
     }
 }
 
@@ -25,4 +29,10 @@ const pizza = new Pizza('Pepperoni', ['small', 'medium', 'large']);
 pizza.addTopping('pepperoni');
 
 console.log(pizza.name);
+console.log(pizza.availableSizes);
+
+pizza.updateSizes(['small', 'medium']);
+console.log(pizza.availableSizes);
+
+pizza.availableSizes = ['small', 'large'];
 console.log(pizza.availableSizes);
